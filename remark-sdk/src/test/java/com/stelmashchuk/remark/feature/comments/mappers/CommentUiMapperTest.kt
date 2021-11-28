6eef3992c7e98666bc17e6b3fefc02572ae2347e
Vova Stelmashchuk <vovochkastelamashchuk@gmail.com>
2021-11-28 23:28:05 +0200

    Fixed tests

--- remark-sdk/src/test/java/com/stelmashchuk/remark/feature/comments/mappers/CommentUiMapperTest.kt
+++ remark-sdk/src/test/java/com/stelmashchuk/remark/feature/comments/mappers/CommentUiMapperTest.kt
@@ -1,6 +1,6 @@
 package com.stelmashchuk.remark.feature.comments.mappers
 
-import com.stelmashchuk.remark.api.repositories.FullComment
+import com.stelmashchuk.remark.api.FullComment
 import io.kotlintest.shouldBe
 import io.mockk.every
 import io.mockk.mockk
